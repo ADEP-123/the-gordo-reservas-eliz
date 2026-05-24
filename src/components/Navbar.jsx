787191898b6327c624f246527cff7366914a39fc
Andres Elizalde <andep210399@gmail.com>
2026-05-24 10:57:19 -0700

    fix: :bug: quitando links del header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,17 +11,6 @@ function Navbar() {
             <p className="navbar-subtitle">Reservas de Mesas</p>
           </div>
         </div>
-        <ul className="navbar-links">
-          <li>
-            <a href="#">Inicio</a>
-          </li>
-          <li>
-            <a href="#">Menú</a>
-          </li>
-          <li>
-            <a href="#">Contacto</a>
-          </li>
-        </ul>
       </div>
     </nav>
   );
